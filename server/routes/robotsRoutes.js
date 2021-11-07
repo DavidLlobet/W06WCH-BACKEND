4482fb9bf140ba680ef8ee738a704369e99f6816
David <david.llobet6@gmail.com>
2021-11-07 18:52:51 +0100

    Implement delete functionality for database

--- server/routes/robotsRoutes.js
+++ server/routes/robotsRoutes.js
@@ -1,10 +1,15 @@
 const express = require("express");
-const { getRobots, getRobotById } = require("../controllers/robotsController");
+const {
+  getRobots,
+  getRobotById,
+  deleteRobotById,
+} = require("../controllers/robotsController");
 
 require("dotenv").config();
 const router = express.Router();
 
 router.get("/", getRobots);
 router.get("/:idRobot", getRobotById);
+router.delete("/delete/:idRobot", deleteRobotById);
 
 module.exports = router;
