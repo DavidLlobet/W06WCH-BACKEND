2354fbb438f51c172edbe0f9f4508ea595c5cc75
David <david.llobet6@gmail.com>
2021-11-08 17:56:21 +0100

    Create userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,22 +1,35 @@
-const loginUser = async (req, res, next) => {
-const {username, password} = req.body;
-if (!user){
- error 401
-} else {
-const rightPassword = await bcrypt.compare(password, user.password); //true o false
-if(!rightPassword){
-	error i tal
-} else {
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-const token = jwt.sign(
-	{
-		id: user.id;
-		name: user.name;
-	}
-	process.env.JWT_SECRET //Me da el token
-		{
-			expireIn
-		}
-	);
-	res.json({token}) //Lo guardo
-}
+const User = require("../../database/models/users");
+
+const loginUser = async (req, res, next) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+  if (!user) {
+    const error = new Error("Not found");
+    error.code = 401;
+    next(error);
+  } else {
+    const rightPassword = await bcrypt.compare(password, user.password);
+    if (!rightPassword) {
+      const error = new Error("Not authorized, wrong password");
+      error.code = 401;
+      next(error);
+    } else {
+      const token = jwt.sign(
+        {
+          id: user.id,
+          name: user.name,
+        },
+        process.env.JWT_SECRET,
+        {
+          expiresIn: 24 * 60 * 60,
+        }
+      );
+      res.json({ token });
+    }
+  }
+};
+
+module.exports = loginUser;
